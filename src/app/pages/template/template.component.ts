4ce61b2afdec8acdfd273d5c6df5d10dc016ecd0
Emanuel Antonio <Emanuel Antonio@DESKTOP-10ULSAH.(none)>
2020-11-08 16:59:52 -0600

    formularios

---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,49 @@
+import { ThrowStmt } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+  usuario = {
+    nombre: '',
+    apellido:'',
+    correo: '',
+    pais: '',
+    genero: 'M'
+  };
+  paises:any[] = [];
+
+  constructor(private paisService:PaisService) { }
+
+  ngOnInit(): void {
+    this.paisService.getPaises()
+      .subscribe(paises=>{
+        // console.log(paises);
+        this.paises = paises;
+        this.paises.unshift({
+          nombre:'[Seleccione pais]',
+          codigo: ''
+        });
+        // console.log(this.paises);
+      });
+  }
+
+  guardar(forma:NgForm){
+    console.log(forma);
+    console.log(forma.value);
+    if(forma.invalid){
+      Object.values(forma.controls).forEach(control=>{
+        // console.log("Control");
+        // console.log(control);
+        control.markAsTouched();
+      });
+      return;
+    }
+  }
+
+}
